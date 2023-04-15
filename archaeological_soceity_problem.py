974fae9b211db94592b7357c7bd32a57b7c98adb
jirenuki-69 <mfuentesmodelo@gmail.com>
2023-04-14 20:46:13 -0600

    Can't resolve excersice 2 :( + headache

--- archaeological_soceity_problem.py
+++ archaeological_soceity_problem.py
@@ -4,11 +4,9 @@ def calculate_year_symbols(year_range):
     A_year, A_type = int(A[:-2]), A[-2:]
     B_year, B_type = int(B[:-2]), B[-2:]
 
-    # Año cero no existe en numeración romana, se trata como año 1BC
     if A_year == 0:
         A_year = 1
 
-    # Función para convertir un número entero en numeración romana
     def int_to_roman(num):
         print(num)
         val = [
@@ -33,31 +31,24 @@ def calculate_year_symbols(year_range):
         print(roman_num)
         return roman_num
 
-    # Calcular el número mínimo de símbolos necesarios para el rango de años
     symbols = 0
 
-    # Años en el mismo tipo (AD o BC)
     if A_type == B_type:
         if A_year <= B_year:
             symbols = len(int_to_roman(B_year - A_year + 1))
         else:
             symbols = len(int_to_roman(A_year - B_year + 1))
 
-    # Años en tipos diferentes (AD y BC)
     else:
-        # Años AD mayores que años BC
         if A_type == 'AD':
             symbols = len(int_to_roman(B_year)) + \
                 len(int_to_roman(A_year))
-        # Años BC mayores que años AD
         else:
             symbols = len(int_to_roman(A_year)) + \
                 len(int_to_roman(B_year))
 
     return symbols
 
-
-# Ejemplos de uso
 year_range_1 = "1BC-1AD"
 result_1 = calculate_year_symbols(year_range_1)
 print("Resultado para", year_range_1, "es:", result_1)
